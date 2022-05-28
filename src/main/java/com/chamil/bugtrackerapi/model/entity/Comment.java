e08dafcd5f8386549467a3301b9c4e2309c3a1bd
chamilp <chamil.prabodha@syscolabs.com>
2022-05-28 23:42:21 +0530

    complete comments controller

--- src/main/java/com/chamil/bugtrackerapi/model/entity/Comment.java
+++ src/main/java/com/chamil/bugtrackerapi/model/entity/Comment.java
@@ -14,4 +14,16 @@ public class Comment extends BaseEntity {
     private Long issueId;
     private String author;
     private String content;
+
+    public Comment merge(Comment comment) {
+        if (comment != null) {
+            if (comment.getContent() != null) {
+                content = comment.getContent();
+            }
+            if (comment.getAuthor() != null) {
+                author = comment.getAuthor();
+            }
+        }
+        return this;
+    }
 }
