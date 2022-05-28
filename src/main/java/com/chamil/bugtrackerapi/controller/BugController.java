11c3585ec5cf0223c0b59bef6646daeb24656e78
chamilp <chamil.prabodha@syscolabs.com>
2022-05-28 22:02:04 +0530

    complete get method in controller

--- src/main/java/com/chamil/bugtrackerapi/controller/BugController.java
+++ src/main/java/com/chamil/bugtrackerapi/controller/BugController.java
@@ -1,14 +1,29 @@
 package com.chamil.bugtrackerapi.controller;
 
+import com.chamil.bugtrackerapi.exception.APIException;
+import com.chamil.bugtrackerapi.model.dto.BugTrackerResponse;
+import com.chamil.bugtrackerapi.model.entity.Bug;
+import com.chamil.bugtrackerapi.service.IssueService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/projects/{projectId}")
 public class BugController {
+    private final IssueService<Bug> bugService;
+
+    @Autowired
+    public BugController(IssueService<Bug> bugService) {
+        this.bugService = bugService;
+    }
 
     @GetMapping("/bugs/{bugId}")
-    public void getBug(@PathVariable Long projectId, @PathVariable Long bugId) {
-
+    public ResponseEntity<BugTrackerResponse<Bug>> getBug(@PathVariable Long projectId, @PathVariable Long bugId) throws APIException {
+        Bug bug = bugService.get(projectId, bugId);
+        BugTrackerResponse<Bug> apiResponse = new com.chamil.bugtrackerapi.model.dto.BugTrackerResponse<>(true, bug);
+        return new ResponseEntity<>(apiResponse, HttpStatus.OK);
     }
 
     @GetMapping("/bugs")
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO project(`id`, `name`, `project_key`) values (1, 'Project 1', 'project-1');
+INSERT INTO bug(`id`, `name`, `description`, `reported_by`, `status`, `severity`, `project_id`) values (10, 'Bug 1', 'Description 1', 'USER 1', 'OPEN', 'LOW', 1);
+INSERT INTO bug(`id`, `name`, `description`, `reported_by`, `status`, `severity`, `project_id`) values (20, 'Bug 2', 'Description 2', 'USER 2', 'CLOSED', 'MEDIUM', 1);
